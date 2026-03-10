70b6935d92c1f8e615e51f3a1691741a26c4ab88
srilakshmi535 <srilakshmigudavarthi@gmail.com>
2026-03-10 09:45:10 +0530

    3rd day

---
+++ tns-java/src/com/tnsif/sample/programs/accessmodifiers/second/class2.java
@@ -0,0 +1,11 @@
+package com.tnsif.programs.accessmodifiers.second;
+
+import com.tnsif.programs.accessmodifiers.first.*;
+
+public class class2 {
+	public static void main(String args[])
+	{
+		sameClass sc1 = new sameClass();
+		System.out.println(sc1.pubvari);
+	}
+}
