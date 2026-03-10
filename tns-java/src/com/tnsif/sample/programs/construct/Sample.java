73dc1b77ee161b60d5f442d0212a359085bc773f
srilakshmi535 <srilakshmigudavarthi@gmail.com>
2026-03-10 09:46:24 +0530

    2nd day

---
+++ tns-java/src/com/tnsif/sample/programs/construct/Sample.java
@@ -0,0 +1,16 @@
+package com.tnsif.programs.construct;
+
+public class Sample {
+	int stdid;
+	String stdname;
+	int smarks;
+	
+	Sample()
+	{
+		System.out.println("This is default con");
+	}
+
+	
+	
+	
+}
