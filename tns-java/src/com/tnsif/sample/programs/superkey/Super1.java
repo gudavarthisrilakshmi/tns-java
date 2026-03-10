4de23a0019782a629d6e7bf7f9f87a58d69ba241
srilakshmi535 <srilakshmigudavarthi@gmail.com>
2026-03-10 09:56:47 +0530

    3rd day

---
+++ tns-java/src/com/tnsif/sample/programs/superkey/Super1.java
@@ -0,0 +1,20 @@
+package com.tnsif.programs.superkey;
+
+public class Super1 {
+	int a = 10;
+	public void display()
+	{
+		System.out.println("this is parent");
+	}
+}
+class Super2 extends Super1{
+	int a=20;
+	
+	public void display()
+	{
+		System.out.println("this is child");
+		super.display();
+		System.out.println(a); //20
+		System.out.println(super.a); //10
+	}
+}
